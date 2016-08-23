538c00ade39981a062b3719e2a1803757a19d8b5
wpelczar <wpelczar1@gmail.com>
2016-08-23 23:19:31 +0200

    add missing script (search)

--- js/main.js
+++ js/main.js
@@ -17,4 +17,17 @@ $(function(){
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
   })
+
+  $('a[href="#search"]').on('click', function (event) {
+    event.preventDefault();
+    $('#search').addClass('open');
+    $('#search > form > input[type="search"]').focus();
+  });
+
+  $('#search button.close').on('click keyup', function (event) {
+    if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+      $('#search').removeClass('open');
+    }
+  });
+
 }) 
