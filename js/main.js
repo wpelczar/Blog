ea8d439ffbee0c997600bb1e47b2e0035f278e93
wpelczar <wpelczar1@gmail.com>
2016-08-24 22:43:26 +0200

    simple post search WIP

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(function(){
   $('a[href="#search"]').on('click', function (event) {
     event.preventDefault();
     $('#search').addClass('open');
-    $('#search > form > input[type="search"]').focus();
+    $('#search-input').focus();
   });
 
   $('#search button.close').on('click keyup', function (event) {
