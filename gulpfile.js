9c1748e40a1dc1cef1ec41894cce39b9cdce8019
wpelczar <wpelczar1@gmail.com>
2016-12-10 20:46:44 +0100

    gulpfilie improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 var gulp = require('gulp');
-var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
+var exec = require('child_process').exec;
 
-gulp.task("build", shell.task([
-  "bundler exec jekyll build --config _config.yml,_configDev.yml --watch"
-]));
+gulp.task('build', function (cb) {
+  exec('bundle exec jekyll build --config _config.yml,_configDev.yml', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
 
-gulp.task("browser-sync", function(){
+gulp.task('rebuild', ['build'], function(){
+  browserSync.reload();
+})
+
+gulp.task("browser-sync", ['build'], function(){
   browserSync.init({
     server: "_site",
     port: 4000
   })
-  
-  gulp.watch("_site/**/*.*").on('change', browserSync.reload);
 });
 
-gulp.task('default', ["build", "browser-sync"]);
+gulp.task('watch', function () {
+    gulp.watch(['index.html', '_layouts/*.html', '_sass/*.*', '_posts/*.md', 'articles/*.html', 'profile/*.html', 'work/*.html', 'js/**/*', 'images/*'], ['rebuild']);
+});
+
+gulp.task('default', ["browser-sync", "watch"]);
